2ea8860a23c853eb53f8c7d0141a85ff81cdc282
Tim Olsen <olsen.tim@gmail.com>
2020-01-16 08:49:04 -0800

    Initial Commit

---
+++ test app 435/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test app 435
+//
+//  Created by Tim Olsen on 1/16/20.
+//  Copyright © 2020 Tim Olsen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
